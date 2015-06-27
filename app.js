8de0192e785d0a22fdbb94e611c60cf1ecc385e7
nakagaw <yy.nw.yy@gmail.com>
2015-06-27 14:47:22 +0900

    ver.0

--- app.js
+++ app.js
@@ -2,43 +2,16 @@
 
 var express = require('express')
     , app = express()
-    // , fs = require('fs')
-    // , mime= require('mime')
-    // , url  = require("url")
-    // , path = require("path")
     , root = "./public/"
     , port = process.env.PORT || 8080 // process.env.PORT はユーザー環境のポート番号でのコネクションの受け入れ
     , WebSocketServer = require('ws').Server
     ;
 
 
-app.use(express.static(root ));
+app.use(express.static(root));
 var server = require('http').createServer(app)
     , WSS = new WebSocketServer({ "server" : server })
 
-// // Create HTTP Server
-// 参考：<a href="http://blog.myon.info/blog/2013-10-12/entry/" target="_blank">Node.jsで遊んだ (2) | とさいぬの隠し部屋</a>
-// server.on('request', function (req, res) {
-//   // Check File Path
-//   var path;
-//   if(req.url == '/') { // req.urlはアクセスされたURLを取得取得
-//     path = root + './index.html';
-//   }
-//   else {
-//     path = '.' + req.url;
-//   }
-//   // Read File and Write
-//   fs.readFile(path, function (err, data) {
-//     if(err) {
-//       res.writeHead(404, {"Content-Type": "text/plain"});
-//       return res.end(req.url + ' not found.');
-//     }
-//     var type = mime.lookup(path);
-//     res.writeHead(200, {"Content-Type": type});
-//     res.write(data);
-//     res.end();
-//   });
-// })
 server.listen(port);
 console.log('http://localhost:' + port);
 
